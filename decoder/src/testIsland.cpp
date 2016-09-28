e7f4b1c678269d94c1c1567f84883e7c0cde730f
Chao Peng <pengc2010@gmail.com>
2016-09-28 17:25:03 -0400

    update the test island example

--- decoder/src/testIsland.cpp
+++ decoder/src/testIsland.cpp
@@ -1,76 +1,103 @@
-//============================================================================//
-// Start Qt and root applications                                             //
-//                                                                            //
-// Chao Peng                                                                  //
-// 02/27/2016                                                                 //
-//============================================================================//
-
 #include "PRadDataHandler.h"
 #include "PRadDSTParser.h"
 #include "PRadEvioParser.h"
 #include "PRadBenchMark.h"
 #include "PRadDAQUnit.h"
 #include "PRadGEMSystem.h"
+#include "PRadIslandWrapper.h"
+#include "TFile.h"
+#include "TTree.h"
 #include <iostream>
 #include <string>
 #include <vector>
+
+#define MAXHIT 50
 
 using namespace std;
 
 int main(int /*argc*/, char * /*argv*/ [])
 {
+  PRadDataHandler *handler = new PRadDataHandler();
+  PRadDSTParser *dst_parser = new PRadDSTParser(handler);
+  
+  handler->ReadConfig("config.txt");
 
-    PRadDataHandler *handler = new PRadDataHandler();
-    PRadDSTParser *dst_parser = new PRadDSTParser(handler);
+  //handler->UpdateHyCalGain("../config/gainfunc_round7.dat");
 
-    // read configuration files
-    handler->ReadConfig("config.txt");
+  dst_parser->OpenInput("/work/hallb/prad/replay/prad_001291.dst");
 
-    PRadBenchMark timer;
-//    handler->ReadFromDST("test.dst");
-//    handler->WriteToDST("test.dst");
+  //----------init root file-------------//
+  TFile *f = new TFile("test5.root", "RECREATE");
+  TTree *t = new TTree("T", "T");
 
-    // here shows an example how to read DST file while not saving all the events
-    // in memory
-    dst_parser->OpenInput("/work/hallb/prad/replay/prad_001287.dst");
+  Int_t clusterN, eventNumber;
+  Double_t Ebeam;
+  Double_t totalE;
+  Double_t clusterE[MAXHIT], clusterX[MAXHIT], clusterY[MAXHIT], clusterChi2[MAXHIT];
+  Int_t clusterNHit[MAXHIT];
+  Int_t clusterStatus[MAXHIT];
+  Int_t clusterType[MAXHIT];
 
-    int count = 0;
+  t->Branch("EventNum", &eventNumber, "EventNum/I");
+  t->Branch("nHyCalHit", &clusterN, "nHyCalHit/I");
+  t->Branch("EBeam", &Ebeam, "EBeam/D");
+  t->Branch("TotalE", &totalE, "TotalE/D");
+  t->Branch("HyCalHitX", &clusterX[0], "HyCalHitX[nHyCalHit]/D");
+  t->Branch("HyCalHitY", &clusterY[0], "HyCalHitY[nHyCalHit]/D");
+  t->Branch("HyCalHitE", &clusterE[0], "HyCalHitE[nHyCalHit]/D");
+  t->Branch("HyCalHitChi2", &clusterChi2[0], "HyCalHitChi2[nHyCalHit]/D");
+  t->Branch("HyCalHitNModule", &clusterNHit[0], "HyCalHitNModule[nHyCalHit]/I");
+  t->Branch("HyCalHitStatus", &clusterStatus[0], "HyCalHitStatus[nHyCalHit]/I");
+  t->Branch("HyCalHitType", &clusterType[0], "HyCalHitType[nHyCalHit]/I");
+  
 
-    // uncomment next line, it will not update calibration factor from dst file
-    //dst_parser->SetMode(NO_HYCAL_CAL_UPDATE);
+  //-------------------------------------//
 
-    while(dst_parser->Read() && count < 30000)
-    {
-        if(dst_parser->EventType() == PRad_DST_Event) {
-            ++count;
-            // you can push this event into data handler
-            // handler->GetEventData().push_back(dst_parser->GetEvent()
-            // or you can just do something with this event and discard it
-            auto event = dst_parser->GetEvent();
-            cout << event.event_number << "  ";
-            cout << event.gem_data.size() << "  ";
-            cout << handler->GetEPICSValue("MBSY2C_energy", event) << endl;
-            for(auto &hit : handler->GetHyCalCluster(event))
-            {
-                cout << hit.E << "  " << hit.x << "  " << hit.y << endl;
-            }
-        } else if(dst_parser->EventType() == PRad_DST_Epics) {
-            // save epics into handler, otherwise get epicsvalue won't work
-            handler->GetEPICSData().push_back(dst_parser->GetEPICSEvent());
-        }
+  int count = 0;
+
+  while(dst_parser->Read()){
+  if(dst_parser->EventType() == PRad_DST_Event) {
+    auto event = dst_parser->GetEvent();
+    if (! (event.trigger == PHYS_LeadGlassSum || event.trigger == PHYS_TotalSum)) continue;
+    if (count%10000 == 0) cout<<"----------event "<<count<<"-------------"<<endl;
+    if (count > 1000000) break; 
+    count++;
+
+    HyCalHit *clusterArray = handler->GetHyCalCluster(clusterN);
+
+    Ebeam = handler->GetEPICSValue("MBSY2C_energy", eventNumber);
+    totalE = 0.;
+    eventNumber = handler->GetCurrentEventNb(); 
+    
+
+    for (int i=0; i<clusterN ; i++){
+      HyCalHit *thisCluster = &clusterArray[i];
+      clusterX[i] = thisCluster->x_log;
+      clusterY[i] = thisCluster->y_log;
+      clusterE[i] = thisCluster->E;
+      totalE += clusterE[i];
+      clusterNHit[i] = thisCluster->nblocks;
+      clusterChi2[i] = thisCluster->chi2;
+      clusterStatus[i] = thisCluster->status;
+      clusterType[i] = thisCluster->type;
     }
 
-    dst_parser->CloseInput();
+    t->Fill();
+    if (count == -1) break;
 
-    cout << "TIMER: Finished, took " << timer.GetElapsedTime() << " ms" << endl;
-    cout << "Read " << handler->GetEventCount() << " events and "
-         << handler->GetEPICSEventCount() << " EPICS events from file."
-         << endl;
-    cout << handler->GetBeamCharge() << endl;
-    cout << handler->GetLiveTime() << endl;
+    
+  } else if(dst_parser->EventType() == PRad_DST_Epics){
+      // save epics into handler, otherwise get epicsvalue won't work
+      handler->GetEPICSData().push_back(dst_parser->GetEPICSEvent());
+  }
 
-//    handler->WriteToDST("prad_001323_0-10.dst");
-    //handler->PrintOutEPICS();
-    return 0;
+  }
+
+  dst_parser->CloseInput();
+
+  f->cd();
+  t->Write();
+  f->Close();
+
+  return 0; 
 }
-
